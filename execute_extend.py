060a1ea31afc877e3b9468b8677cf540706914db
lequocanhlhu122001209 <quocanhpentakill@gmail.com>
2026-05-08 07:02:43 +0700

    Remove Word export functionality and python-docx dependency

--- execute_extend.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, 'c:\\Users\\HP\\Music\\classifies-students')
-
-# Run the extend script
-from scripts.extend_word_to_40_pages import main
-
-if __name__ == "__main__":
-    main()
--- run_word_generator.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "c:\Users\HP\Music\classifies-students"
-"c:/Users/HP/Music/classifies-students/.venv/Scripts/python.exe" generate_word_doc.py
-pause
